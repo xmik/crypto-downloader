e1081aa177d480ee88d01eec389e47d497a9e44f
hadrrb <hadrichb@gmail.com>
2019-04-07 12:04:13 +0200

    saving implemented. initial version. still there is cancellationToken to add. normal behaviour ok, but does not pass the unit tests

--- src/CryptoDownloaderConsole/TimeSeries/EventSavingService.cs
+++ src/CryptoDownloaderConsole/TimeSeries/EventSavingService.cs
@@ -1,5 +1,8 @@
 using System;
+using System.IO;
+using System.Text;
 using System.Threading;
+using System.Linq;
 
 namespace CryptoDownloader
 {
@@ -12,8 +15,40 @@ namespace CryptoDownloader
 
         public int Write(CandleEvent[] candleEvents, string filePath, CancellationToken ct, IRange<NodaTime.Instant> allowedDateRange)
         {
-            throw new NotImplementedException();
+			int i = 0;
+            NodaTime.Instant lasttimestamp = NodaTime.Instant.FromDateTimeUtc(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc));
+            if (File.Exists(filePath))
+            {
+                string lastline = File.ReadLines(filePath, Encoding.UTF8).Last();
+                lastline = lastline.Substring(0, lastline.IndexOf(','));
+                DateTime lasttime = DateTime.ParseExact(lastline, "yyyy-M-ddTHH:mm:ss", null);
+                lasttimestamp = DateTimeExtensions.ToNodaTime(lasttime.ToUniversalTime());
+            }
+            else
+            {
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(filePath, false))
+                {
+                    file.WriteLine("<date time>,<open price>,<high price>,<low price>,<close price>");
+                }
+            }
+
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(filePath, true))
+            {
+                foreach (var ce in candleEvents)
+                {
+                    var candletime = (new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).AddTicks(ce.Date);
+                    if (i == 0 & candletime.Equals(lasttimestamp)) continue;
+                    if (allowedDateRange.Includes(DateTimeExtensions.ToNodaTime(candletime)))
+                    {
+                        file.WriteLine("{0},{1}",
+                            candletime.ToString("yyyy-M-ddTHH:mm:ss"),
+                            ce.Value.ToString());
+                    }
+                    i++;
+                }
+            }
             _log.DebugFormat("Finished writing to series, added: {0} events", candleEvents.Length);
+            return i;
         }
     }
 }
